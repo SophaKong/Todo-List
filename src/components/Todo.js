2625b05ff5e99006d41af389a8fb3255d099e86c
SophaKong <sophakong222@gmail.com>
2021-12-04 09:46:04 +0700

    3 files modified

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -39,3 +39,5 @@ function Todo() {
 }
 
 export default Todo;
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import Todo from './components/Todo';
 
-import App from "./App";
+import App from './App';
 
 // ReactDOM.render(<App />, document.getElementById("root"));
-ReactDOM.render(<Todo />, document.getElementById("root"));
+ReactDOM.render(<Todo />, document.getElementById('root'));
