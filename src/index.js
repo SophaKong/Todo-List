60163317c6bffb3fbc8012172559b6d9ff996abc
SophaKong <sophakong222@gmail.com>
2021-12-04 09:37:26 +0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
+import Todo from './components/Todo';
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Todo />, document.getElementById("root"));
